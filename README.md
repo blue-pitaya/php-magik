c0496467fa9b88c66e4bc24eaa201b7a3ddc0c06
blue.pitaya <blue.pitaya@pm.me>
2026-07-15 18:38:25 +0200

    added GPLv3 license

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# php-lsp
+# php-magik
 
 ## Test
 
--- src/app_ctx.c
+++ src/app_ctx.c
@@ -1,3 +1,20 @@
+// Copyright (C) 2026 blue-pitaya
+//
+// This file is part of php-magik.
+//
+// php-magik is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// php-magik is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with php-magik. If not, see <https://www.gnu.org/licenses/>.
+
 #include "app_ctx.h"
 #include "function.h"
 #include "var.h"
